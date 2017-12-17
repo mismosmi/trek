3e1b756fc5d76bed4ced81b299a36a5ba332d4f2
Michel Smola <m@michelsmola.de>
2017-12-17 14:54:49 +0100

    jquery,bootstrap,index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>TREK</title>
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="../css/main.css">
+<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../js/jquery.min.js"></script>
+<script type="text/javascript" src="../js/main.js"></script>
+
+</head>
+<body>
+</body>
+</html>
